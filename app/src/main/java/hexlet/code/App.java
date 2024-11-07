381d57975a3618b31c738be39bc1076b426ba2d7
Zakharov1221 <zakharovmo92@gmail.com>
2024-11-07 21:05:46 +0300

    update App by swtich-case and update Cli add evenGame

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,12 +1,37 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
-        Cli cli = new Cli();
-        cli.askName();
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 – Greet");
+        System.out.println("2 – Even");
+        System.out.println("0 – Exit");
 
+        Scanner numberOfGame = new Scanner(System.in);
+        int choice = numberOfGame.nextInt();
+
+        System.out.println("Your choice " + choice);
+
+        switch (choice) {
+            case 1:
+                Cli cli1 = new Cli();
+                cli1.askName();
+                break;
+
+            case 2:
+                Cli cli2 = new Cli();
+                cli2.evenGame();
+                break;
+
+            case 0:
+                break;
+
+            default:
+                System.out.println("Wrong command.");
+                break;
+            }
     }
-
-
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,12 +4,53 @@ import java.util.Scanner;
 
 public class Cli {
 
-    public void askName() {
+    public String askName() {
 
         System.out.print("May I have your name? ");
         Scanner console = new Scanner(System.in);
         String name = console.nextLine();
         System.out.println("Hello, " + name + ".");
 
+        return name;
+    }
+
+    public void evenGame(){
+
+        Cli gameUser2 = new Cli();
+        String name = gameUser2.askName();
+        String correctAnswer = "yes";
+        String incorrectAnswer = "no";
+        int gameCount = 3;
+
+        Scanner console = new Scanner(System.in);
+
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'");
+
+        for (int i = 1; i <= gameCount; i = i + 1) {
+            var randomNumber = (int) (Math.floor(Math.random() * 98) + 1);
+            System.out.println("Question: " + randomNumber);
+            String answer = console.nextLine();
+            System.out.println("Your answer: " + answer);
+
+            if (randomNumber % 2 == 0 && answer.equals(correctAnswer) || randomNumber % 2 != 0 && answer.equals(incorrectAnswer)){
+                System.out.println("Correct!");
+
+            } else if (randomNumber % 2 != 0 && answer.equals(correctAnswer)){
+                System.out.println("'yes' wrong answer ;(. Correct answer was 'no'.");
+                System.out.println("Let's try again, " + name);
+                break;
+
+            } else if (randomNumber % 2 == 0 && answer.equals(incorrectAnswer)){
+                System.out.println("'no' wrong answer ;(. Correct answer was 'yes'.");
+                System.out.println("Let's try again, " + name);
+                break;
+
+            } else {
+                System.out.println("Wrong answer anyway!");
+                System.out.println("Let's try again, " + name);
+                break;
+            }
+        }
+        System.out.println("Congratulations, " + name + "!");
     }
 }
