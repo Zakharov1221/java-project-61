5a79f1b36cb419d57f3fd59248c08b4534954a9d
Zakharov1221 <zakharovmo92@gmail.com>
2024-12-09 23:03:29 +0300

    update

--- app/src/main/java/hexlet/code/games/GameProgression.java
+++ app/src/main/java/hexlet/code/games/GameProgression.java
@@ -1,57 +1,42 @@
 package hexlet.code.games;
 
+import hexlet.code.Engine;
+import static hexlet.code.Cli.askName;
 import java.util.Scanner;
 
-import static hexlet.code.Cli.askName;
+import static hexlet.code.Engine.ROUND_COUNT;
 
 public class GameProgression {
 
-    public static void progressionGame(){
+    public static void progressionGame() {
         String name = askName();
         Scanner console = new Scanner(System.in);
 
-        String progression1 = "2 4 6 8 10 12 14 16 18 20";
-        String progression2 = "3 6 9 12 15 18 21 24 27 30";
-        String progression3 = "4 8 12 16 20 24 28 32 36 40";
-
         System.out.println("What number is missing in the progression?");
 
-        System.out.println(progression1.replace("10", ".."));
-        int answer = console.nextInt();
-        System.out.println("Your answer: " + answer);
+        for (int i = 1; i <= ROUND_COUNT; i++) {
+            int length = 10;
+            int step = (int) (Math.floor(Math.random() * 10) + 1);
+            int index = (int) (Math.floor(Math.random() * 10) + 1);
 
-        if (answer == 10) {
-            System.out.println("Correct!");
-        } else {
-            System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" +
-                    "10" + "'.");
-            System.out.println("Let's try again, " + name + "!");
-            return;
-        }
-        System.out.println(progression2.replace("21", ".."));
-        answer = console.nextInt();
-        System.out.println("Your answer: " + answer);
+            StringBuilder progression = new StringBuilder();
 
-        if (answer == 21) {
-            System.out.println("Correct!");
-        } else {
-            System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" +
-                    "21" + "'.");
-            System.out.println("Let's try again, " + name + "!");
-            return;
-        }
-        System.out.println(progression3.replace("36", ".."));
-        answer = console.nextInt();
-        System.out.println("Your answer: " + answer);
+            String result = null;
+            for (int j = 1; j < length; j++) {
+                if (j == index) {
+                    progression.append(" ").append("..");
+                    result = Integer.toString(step * j);
+                } else {
+                    progression.append(" ").append(step * j);
+                }
+            }
 
-        if (answer == 36) {
-            System.out.println("Correct!");
-        } else {
-            System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" +
-                    "36" + "'.");
-            System.out.println("Let's try again, " + name + "!");
-            return;
+            System.out.println("Question " + progression);
+            var answer = console.next();
+            System.out.println("Your answer: " + answer);
+
+            Engine.gameChecker(answer, result, name);
         }
-        System.out.println("Congratulations, " + name + "!");
     }
 }
+
